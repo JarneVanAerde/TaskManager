5f7cfa7f1b2337d7be3a36d5bc6a94299e52c104
Jarne Van Aerde <jarne.vanaerde@hotmail.com>
2023-06-07 18:58:12 +0200

    working on app permissions

--- TaskManager/MauiProgram.cs
+++ TaskManager/MauiProgram.cs
@@ -4,6 +4,7 @@ using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
+using System.Threading.Tasks;
 using TaskManager.ViewModels;
 using TaskManager.Views;
 
--- TaskManager/Platforms/Android/MainActivity.cs
+++ TaskManager/Platforms/Android/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
 using Microsoft.Maui;
 using Microsoft.Maui.ApplicationModel;
 
@@ -17,6 +18,11 @@ public class MainActivity : MauiAppCompatActivity
         base.OnResume();
 
         Platform.OnResume(this);
+    }
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
     }
 
     protected override void OnNewIntent(Android.Content.Intent intent)
