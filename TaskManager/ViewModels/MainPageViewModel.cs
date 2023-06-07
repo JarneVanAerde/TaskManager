286bd219ddb931c95ca260127bf82316277e52d3
Jarne Van Aerde <jarne.vanaerde@hotmail.com>
2023-06-07 19:52:42 +0200

    add stuff for permisson checking

--- TaskManager/ViewModels/MainPageViewModel.cs
+++ TaskManager/ViewModels/MainPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Devices;
 using System.Threading.Tasks;
 
 namespace TaskManager.ViewModels;
@@ -11,13 +13,30 @@ public partial class MainPageViewModel : BaseViewModel
         Title = "Overview";
     }
 
+    // TODO: Extract to permission service
     [RelayCommand]
     private async Task AddTodo()
     {
         var storageWritePermission = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
-        if (storageWritePermission != PermissionStatus.Granted)
+  
+        var shouldShowExplanation =
+            storageWritePermission == PermissionStatus.Denied &&
+            DeviceInfo.Platform == DevicePlatform.Android &&
+            Permissions.ShouldShowRationale<Permissions.StorageWrite>();
+        if (shouldShowExplanation)
         {
-            return;
+            await Shell.Current.DisplayAlert("Important!", "It is important that we get these permission, otherwise you will not be able to add a TODO.", "OK");
+        }
+
+        storageWritePermission = await Permissions.RequestAsync<Permissions.StorageWrite>();
+
+        if (storageWritePermission == PermissionStatus.Granted)
+        {
+            await Shell.Current.DisplayAlert("GRANTED!", "Permission granted!", "OK");
+        }
+        else
+        {
+            throw new Exception("You did not do what we asked...");
         }
     }
 }
